a359d30087cc8715e9bfc3dcb9e0d5bfdb471424
Fernando López <fernandboy18@gmail.com>
2025-08-25 12:52:00 -0600

    Entrega Práctica

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,14 @@
 export function fizzbuzz(){
   //tu código va aqui
-  
+  for(let n = 1; n < 101; n++){
+    
+    if (n % 3 == 15)
+      console.log("Fizz")
+    else if (n % 3 == 0)
+      console.log("BUzz")
+    else if (n % 5 == 0)
+      console.log("FizzBUzz")
+    elseconsole.log(n)
+  }
+  return 0;
 }
