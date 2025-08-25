c2105b8927a59809286815a673edb3c850ad6f62
Fernando López <fernandboy18@gmail.com>
2025-08-25 13:02:32 -0600

    Tercera corrección

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,13 +2,13 @@ export function fizzbuzz(){
   //tu código va aqui
   for(let n = 1; n < 101; n++){
     
-    if (n % 3 == 15)
-      console.log("Fizz")
+    if (n % 15 == 0)
+      console.log("FizzBuzz")
     else if (n % 3 == 0)
-      console.log("BUzz")
+      console.log("Fizz")
     else if (n % 5 == 0)
-      console.log("FizzBUzz")
-    
+      console.log("BUzz")
+
     console.log(n)
   }
 }
