e2270af9d19ba59bd14a5e60973f87c05be5893b
Fernando López <fernandboy18@gmail.com>
2025-08-25 13:08:11 -0600

    Cuarta corrección

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,13 +2,14 @@ export function fizzbuzz(){
   //tu código va aqui
   for(let n = 1; n < 101; n++){
     
-    if (n % 15 == 0)
+    if (n % 15 == 0){
       console.log("FizzBuzz")
-    else if (n % 3 == 0)
+    }else if (n % 3 == 0){
       console.log("Fizz")
-    else if (n % 5 == 0)
+    }else if (n % 5 == 0){
+    }else { 
       console.log("BUzz")
-
+    }
     console.log(n)
   }
 }
